7f64d90c74ed769350c5e3b8146a2ea7e002626a
lixiaojing <lixiaojing@etaostars.com>
2016-02-16 16:28:27 +0800

    提交文件

---
+++ Easy.Monitor.Infrastructure/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easy.Monitor.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
