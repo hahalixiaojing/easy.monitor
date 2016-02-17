2c7ebfc10e56996ed664e401186ae87a7289bee0
ryan <chen.jingnan@etaostars.com>
2016-02-17 15:23:36 +0800

    样式

---
+++ Easy.Monitor/App_Start/RouteConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Easy.Monitor 
+{
+    /// <summary>
+    /// MVC路由配置
+    /// </summary>
+    public class RouteConfig
+    {
+        /// <summary>
+        /// 注册路由
+        /// </summary>
+        /// <param name="routes"></param>
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/",
+                defaults: new { controller = "Home", action = "Index"}
+            );
+        }
+    }
+}
