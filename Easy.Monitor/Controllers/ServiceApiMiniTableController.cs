e8662c5a5ea34729dfbf29b4e8d29028e26c7166
lixiaojing <lixiaojing@etaostars.com>
2016-05-04 13:46:07 +0800

    修改

--- Easy.Monitor/Controllers/ServiceApiMiniTableController.cs
+++ Easy.Monitor/Controllers/ServiceApiMiniTableController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -19,10 +20,10 @@ namespace Easy.Monitor.Controllers
         }
 
         [HttpPost]
-        public ActionResult SelectMetadataByPage(string serviceName, string api, int pageIndex = 1, int pageSize = 100)
+        public JsonResult SelectMetadataByPage(string serviceName, string api, int pageIndex = 1, int pageSize = 100)
         {
             var list = Application.ApplicationRegistry.StatMetaData.SelectFrequency2(serviceName, api, pageIndex, pageSize);
-            return this.PartialView("TableDataTemplate", list);
+            return Json(list);
         }
     }
 }
