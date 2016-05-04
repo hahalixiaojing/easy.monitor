85de4dab09899ba985e59fe96a506474111c778c
lixiaojing <lixiaojing@etaostars.com>
2016-05-04 09:27:59 +0800

    提交文件

--- Easy.Monitor/Controllers/ServiceApiMiniTableController.cs
+++ Easy.Monitor/Controllers/ServiceApiMiniTableController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Easy.Monitor.Utility;
 
 namespace Easy.Monitor.Controllers
 {
+    [WebAuthorize]
     public class ServiceApiMiniTableController : Controller
     {
-        // GET: ServiceApiMiniTable
         public ActionResult Index(string serviceName, string api)
         {
             ViewBag.ServiceName = serviceName;
